106f709f32d5f46f401fdc83d4df03c43f023d0d
Quinn C. Hoyer <quinn@themightyquinn.com>
2014-08-10 03:03:22 -0700

    DRYing it out a bit

--- index.js
+++ index.js
@@ -7,134 +7,87 @@ var TwitterBot = function (config) {
 	this.twit = new Twit(config);
 };
 
+var get = function (path, params, context) {
+	"use strict";
+
+	if (arguments.length === 2) {
+		context = params;
+		params = {};
+	}
+
+	return new RSVP.Promise(function (resolve, reject) {
+		context.twit.get(path, params, function (err, response) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(response);
+			}
+		});
+	});
+};
+
+var post = function (path, params, context) {
+	"use strict";
+
+	if (arguments.length === 2) {
+		context = params;
+		params = {};
+	}
+
+	return new RSVP.Promise(function (resolve, reject) {
+		context.twit.post(path, params, function (err, response) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(response);
+			}
+		});
+	});
+};
+
 TwitterBot.prototype = {
 
 	rateLimitStatus: function () {
 		"use strict";
 
-		return new RSVP.Promise(function (resolve, reject) {
-			this.twit.get('application/rate_limit_status', {}, function (err, response) {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(response);
-				}
-			});
-		}.bind(this));
+		return get('application/rate_limit_status', this);
 	},
 
 	getSlugs: function () {
 		"use strict";
 
-		return new RSVP.Promise(function (resolve, reject) {
-			this.twit.get('users/suggestions', {}, function (err, response) {
-				if (err) {
-					reject(err);
-				} else {
-					resolve(response);
-				}
-			});
-		}.bind(this));
+		return get('users/suggestions', this);
 	},
 
 	suggestUsers: function (slug) {
 		"use strict";
 
-		return new RSVP.Promise(function (resolve, reject) {
-			this.twit.get('users/suggestions/' + slug,
-				{ slug: slug },
-				function (err, response) {
-					if (err) {
-						reject(err);
-					} else {
-						resolve(response);
-					}
-				});
-		}.bind(this));
+		return get('users/suggestions/' + slug, { slug: slug }, this);
 	},
 
-	getFriends: function (callback) {
+	getFriends: function () {
 		"use strict";
 
-		this.twit.get('friends/ids', {}, callback || logResponse);
+		return get('friends/ids', this);
+	},
+
+	getFollowers: function () {
+		"use strict";
+
+		return get('followers/ids', this);
 	},
 
 	follow: function (idStr) {
 		"use strict";
 
-		return new RSVP.Promise(function (resolve, reject) {
-			this.twit.post('friendships/create',
-				{ user_id: idStr },
-				function (err, response) {
-					if (err) {
-						reject(err);
-					} else {
-						resolve(response);
-					}
-				}
-			);
-		}.bind(this));
+		return post('friendships/create', { user_id: idStr }, this);
 
 	},
 
-	retweet: function (idStr, callback) {
+	retweet: function (idStr) {
 		"use strict";
 
-		this.twit.post('statuses/retweet/' + idStr, { id: idStr }, callback || logResponse);
-	},
-
-	retweetMostRecentPhoto: function () {
-		"use strict";
-
-		this.twit.get('statuses/home_timeline', { count: 5 }, function (err, response) {
-			var i, j,
-				photoTweet;
-
-			for (i = 0; i < response.length; i++) {
-				var media = response[i].entities.media;
-
-				for (j = 0; j < media.length; j += 1) {
-					if (media[j].type === 'photo') {
-						photoTweet = response[i].id_str;
-						break;
-					}
-				}
-
-				if (photoTweet) {
-					break;
-				}
-			}
-
-			if (photoTweet) {
-				console.log('Found Photo Tweet: ' + photoTweet);
-				twit.post('statuses/retweet/' + photoTweet, { id: photoTweet }, logResponse);
-			} else {
-				console.log('None Found');
-			}
-		});
-	},
-
-	addNewUsers: function (howMany, searchTerm) {
-		"use strict";
-
-		//Do user search
-		this.twit.get('users/search', { q: searchTerm }, function (err, response) {
-
-			var newUserCount = 0;
-			var newUsers = [];
-
-			response.forEach(function (user) {
-				if (!user.following && (newUserCount < howMany)) {
-					// Add user as follow
-					this.twit.post('friendships/create', { user_id: user.id }, logResponse);
-
-					newUsers.push(user.name);
-					newUserCount += 1;
-				}
-			});
-
-			console.log('Added the following users: ' + newUsers);
-		});
+		return post('statuses/retweet/' + idStr, { id: idStr }, this);
 	}
 };
 
